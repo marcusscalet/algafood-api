1c9012ac0d5c51fce2f5e1b4a633582f060abb76
Marcus Scalet <marcus.scalet@hotmail.com>
2020-11-23 16:37:49 -0300

    Customizado o corpo da resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/marcusscalet/algafood/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/marcusscalet/algafood/exceptionhandler/ApiExceptionHandler.java
@@ -2,52 +2,61 @@ package com.marcusscalet.algafood.exceptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.marcusscalet.algafood.domain.exception.EntityInUseException;
 import com.marcusscalet.algafood.domain.exception.EntityNotFoundException;
 import com.marcusscalet.algafood.domain.exception.GenericException;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<?> handlingEntityNotFoundException(EntityNotFoundException e) {
-		Problem problem = Problem.builder()
-				.dateTime(LocalDateTime.now())
-				.message(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problem);
-	}
-	
-	@ExceptionHandler(EntityInUseException.class)
-	public ResponseEntity<?> handlingEntityInUseException(EntityInUseException e){
-		Problem problem = Problem.builder()
-				.dateTime(LocalDateTime.now())
-				.message(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(problem);
-	}
-	
-	@ExceptionHandler(GenericException.class)
-	public ResponseEntity<?> handlingGenericException(GenericException e) {
-		Problem problem = Problem.builder()
-				.dateTime(LocalDateTime.now())
-				.message(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problem);
+	public ResponseEntity<?> handlingEntityNotFoundException(
+			EntityNotFoundException ex, WebRequest request) {
+
+		return handleExceptionInternal(
+				ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 
-	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-	public ResponseEntity<?> handlingHttpMediaTypeNotSupportedException(){
-		Problem problem = Problem.builder()
-				.dateTime(LocalDateTime.now())
-				.message("O tipo de mídia não é aceito").build();
+	@ExceptionHandler(EntityInUseException.class)
+	public ResponseEntity<?> handlingEntityInUseException(
+			EntityInUseException ex, WebRequest request) {
+
+		return handleExceptionInternal(
+				ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+	}
+
+	@ExceptionHandler(GenericException.class)
+	public ResponseEntity<?> handlingGenericException(GenericException ex, WebRequest request) {
+
+		return handleExceptionInternal(
+				ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
 		
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problem);
+		if (body == null) {
+			
+		body = Problem.builder()
+				.dateTime(LocalDateTime.now())
+				.message(status.getReasonPhrase())
+				.build();
+		
+		} else if(body instanceof String) {
+			body = Problem.builder()
+					.dateTime(LocalDateTime.now())
+					.message((String) body)
+					.build();
+		}
+		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 }
