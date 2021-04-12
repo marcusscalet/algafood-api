ecb4c62ca24e1ae776c91e4e894009764dfa5e3c
Marcus Scalet <marcus.scalet@hotmail.com>
2021-04-12 11:00:52 -0300

    Configurado o Resource Server para JWT com chave simétrica

--- src/main/java/com/marcusscalet/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/marcusscalet/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.marcusscalet.algafood.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +22,14 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 			.and()
 				.cors()
 			.and()
-				.oauth2ResourceServer().opaqueToken();
+				.oauth2ResourceServer().jwt();
+	}
+	
+	@Bean
+	public JwtDecoder  jwtDecoder() {
+		var secretKey = new SecretKeySpec("f8ds67fs4d6gh3dfgfdsghe33j1tr46hfrvc348mghn".getBytes(), "HmacSHA256");
+		
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 	
 }
